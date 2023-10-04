4612e1ca09ad74630684582f95728d1ecd5f5c6b
VictorYabuki <127402953+VictorYabuki@users.noreply.github.com>
2023-10-04 08:06:31 -0400

    Add files via upload

---
+++ tccc/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBKveEJz1ZZ1mmks4HI-01a0_vK89ROKQ0",
+  authDomain: "controle-de-gastos-20523.firebaseapp.com",
+  projectId: "controle-de-gastos-20523",
+  storageBucket: "controle-de-gastos-20523.appspot.com",
+  messagingSenderId: "458497068113",
+  appId: "1:458497068113:web:c8a9925e96846751437c70"
+};
+firebase.initializeApp(firebaseConfig);
